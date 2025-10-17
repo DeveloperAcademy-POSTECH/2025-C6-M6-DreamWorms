702d57bc9a9072c367196316c172c71ae0adda60
mini-min <mini@miniui-MacBookAir.local>
2025-10-17 23:39:21 +0900

    [Setting] #3 - Documentation 파일 추가

--- DreamWorms-iOS/DreamWorms-iOS/Application/Coordinator/AppCoordinator.swift
+++ DreamWorms-iOS/DreamWorms-iOS/Application/Coordinator/AppCoordinator.swift
@@ -12,24 +12,24 @@ import SwiftUI
 final class AppCoordinator: ObservableObject {
     @Published var path: [AppRoute] = []
 
-    /// push : 다음 화면으로 넘어갈 때 사용하는 메서드 (_ route 부분에 전환하고자 하는 다음 화면 명시)
+    /// 다음 화면으로 넘어갈 때 사용하는 메서드 (_ route 부분에 전환하고자 하는 다음 화면 명시)
     func push(_ route: AppRoute) {
         path.append(route)
     }
 
-    /// pop : 이전 화면으로 돌아갈 때 사용하는 메서드
+    /// 이전 화면으로 돌아갈 때 사용하는 메서드
     func pop(_ steps: Int = 1) {
         guard steps > 0, !path.isEmpty else { return }
         let stepsToRemove = min(steps, path.count)
         path.removeLast(stepsToRemove)
     }
 
-    /// popToRoot : path에 쌓여있는 모든 화면을 지우고, 루트로 돌아가도록 하는 메서드
+    /// path에 쌓여있는 모든 화면을 지우고, 루트로 돌아가도록 하는 메서드
     func popToRoot() {
         path.removeLast(path.count)
     }
 
-    /// replaceLast : 현재 화면을 새로운 화면으로 바꿀 때 사용하는 메서드
+    /// 현재 화면을 새로운 화면으로 바꿀 때 사용하는 메서드
     func replaceLast(with route: AppRoute) {
         if !path.isEmpty {
             path.removeLast()
