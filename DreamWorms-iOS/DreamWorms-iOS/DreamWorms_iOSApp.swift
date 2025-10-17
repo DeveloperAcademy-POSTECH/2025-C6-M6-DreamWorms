8a2c8d36d4f364d38a72e49aead4ce217f0addb7
Moo <mutopia82@gmail.com>
2025-10-17 12:55:28 +0900

    Initial commit

---
+++ DreamWorms-iOS/DreamWorms-iOS/DreamWorms_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamWorms_iOSApp.swift
+//  DreamWorms-iOS
+//
+//  Created by Moo on 10/15/25.
+//
+
+import SwiftUI
+
+@main
+struct DreamWorms_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
