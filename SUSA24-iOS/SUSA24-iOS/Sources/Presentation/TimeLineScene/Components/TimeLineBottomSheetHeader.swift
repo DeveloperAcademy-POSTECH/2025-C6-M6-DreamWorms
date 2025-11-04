8debb6d6b6cc2aff7d82df9772299cd652bb880a
G.M_YOO <ykm7003@gmail.com>
2025-11-04 19:11:31 +0900

    [Feat] #34 - TimeLineScene Components 폴더 생성
    why ?
    - 이후 바텀시트에 사용할 Components 분리를 위해서
    
    How ?
    - TimeLineScene/Components/TimeLineBottomSheetHeader.swift 파일 추가함.

---
+++ SUSA24-iOS/SUSA24-iOS/Sources/Presentation/TimeLineScene/Components/TimeLineBottomSheetHeader.swift
@@ -0,0 +1,16 @@
+//
+//  TimeLineBottomSheetHeader.swift
+//  SUSA24-iOS
+//
+//  Created by Demian Yoo on 11/4/25.
+//
+
+import SwiftUI
+
+struct TimeLineBottomSheetHeader: View {
+    var body: some View {
+        VStack {
+            Text(.bottomSheetCaseLocationPinCount(countNumber: 2))
+        }
+    }
+}
