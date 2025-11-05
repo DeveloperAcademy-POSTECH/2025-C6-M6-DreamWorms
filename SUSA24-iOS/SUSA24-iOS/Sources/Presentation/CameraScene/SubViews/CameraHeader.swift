e0686246a50007fc2484f9e7e00d443c6567d347
TAENI <52228832+delightPIP@users.noreply.github.com>
2025-11-05 18:08:58 +0900

    [Feat] #39 - Add CameraHeader
    What?
    Camera 상단 헤더 버튼을 구현했습니다.

---
+++ SUSA24-iOS/SUSA24-iOS/Sources/Presentation/CameraScene/SubViews/CameraHeader.swift
@@ -0,0 +1,38 @@
+//
+//  CameraHeader.swift
+//  SUSA24-iOS
+//
+//  Created by taeni on 11/5/25.
+//
+
+import SwiftUI
+
+struct CameraHeader: View {
+    let onBackTapped: () -> Void
+    let onScanTapped: () -> Void
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            HStack {
+                DWCircleButton(
+                    image: Image(.back),
+                    action: onBackTapped
+                )
+                Spacer()
+                DWGlassEffectCircleButton(
+                    action: onScanTapped,
+                    icon: Image(.checkmark)
+                )
+                .setupSize(52)
+                .setupIconColor(.labelColorNormal)
+                .setupInteractiveEffect(true)
+                .setupbuttonBackgroundColor(.primaryNormal)
+            }
+        }
+        .padding(.horizontal, 16)
+    }
+}
+
+//#Preview {
+//    CameraHeader(onBackTapped: {}, onScanTapped: {})
+//}
