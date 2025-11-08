927048f0304ad2d04363cb71214f582671242b2c
Moo <mutopia82@gmail.com>
2025-11-08 22:27:05 +0900

    [Feat] #60 - Adding a coordinate structure model and adopting existing model Equatable
    What?
    - 타입 안정성을 위해 네이버 좌표에 맞는 구조체 모델을 만들었습니다.
    - 기존 PlaceInfo의 타입비교를 위해 Equatable 프로토콜을 추가했습니다.

--- SUSA24-iOS/SUSA24-iOS/Sources/Presentation/MapScene/Model/PlaceInfo.swift
+++ SUSA24-iOS/SUSA24-iOS/Sources/Presentation/MapScene/Model/PlaceInfo.swift
@@ -2,10 +2,10 @@
 //  PlaceInfo.swift
 //  SUSA24-iOS
 //
-//  Created by Moo on 11/6/25.
+//  Updated by Moo on 11/08/25.
 //
 
-struct PlaceInfo {
+struct PlaceInfo: Equatable {
     /// title은 장소가 있으면 건물명, 없으면 도로명으로 대체
     let title: String
     let jibunAddress: String
