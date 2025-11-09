0fb140dc8d9807868d45aac931c40354183ba28f
Moo <mutopia82@gmail.com>
2025-11-09 17:01:39 +0900

    [Chore] #69 - cctv, layer 뷰에 쓰일 Assets 추가 및 리터럴 업데이트
    What?
    - SVG로 모두 추가했습니다. 싱글 스케일 적용 완료.
    - String 리터럴 다 업데이트 했어요.
    - 폰트 적용 안된 거 하나 추가했습니다.!!
    
    Why?
    - 커버리지 이미지를 PNG를 해야하나!!!! 아님 그려야하나 !! 싶었는데 SVG로 올려보니 괜찮아서 이대로 갑니다.

--- SUSA24-iOS/SUSA24-iOS/Resources/Literals/FontLiterals.swift
+++ SUSA24-iOS/SUSA24-iOS/Resources/Literals/FontLiterals.swift
@@ -47,6 +47,7 @@ extension Font {
 
     static let numberMedium16: Font = .custom(FontName.pretendardMedium.rawValue, size: 16)
     static let numberMedium15: Font = .custom(FontName.pretendardMedium.rawValue, size: 15)
+    static let numberRegular13: Font = .custom(FontName.pretendardRegular.rawValue, size: 13)
     static let numberRegular15: Font = .custom(FontName.pretendardRegular.rawValue, size: 15)
     static let numberSemiBold14: Font = .custom(FontName.pretendardSemiBold.rawValue, size: 14)
 }
@@ -78,6 +79,7 @@ extension UIFont {
     
     static let numberMedium16: UIFont = .custom(.pretendardMedium, size: 16)
     static let numberMedium15: UIFont = .custom(.pretendardMedium, size: 15)
+    static let numberRegular13: UIFont = .custom(.pretendardRegular, size: 13)
     static let numberRegular15: UIFont = .custom(.pretendardRegular, size: 15)
     static let numberSemiBold14: UIFont = .custom(.pretendardSemiBold, size: 14)
 }
