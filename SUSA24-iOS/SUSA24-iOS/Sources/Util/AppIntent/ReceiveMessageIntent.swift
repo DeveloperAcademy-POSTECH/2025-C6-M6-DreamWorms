b8d66da59b5daa8bf54a8c991fce89b12f3508eb
G.M_YOO <ykm7003@gmail.com>
2025-11-11 02:06:17 +0900

    [Feat] #84 AppIntent 채택
    why ?
    
    기지국 위치정보를 자동으로 받아오기위해서 App Intent를 채택함.
    
    How ?
    
    title/description 설정

--- SUSA24-iOS/SUSA24-iOS/Sources/Util/AppIntent/ReceiveMessageIntent.swift
+++ SUSA24-iOS/SUSA24-iOS/Sources/Util/AppIntent/ReceiveMessageIntent.swift
@@ -2,7 +2,20 @@
 //  ReceiveMessageIntent.swift
 //  SUSA24-iOS
 //
-//  Created by mini on 10/31/25.
+//  Created by Demian Yoo on 11/10/25.
 //
 
+import AppIntents
+import CoreData
 import Foundation
+
+/// 기지국에서 보낸 문자 메시지를 받아 위치 정보를 자동으로 저장하는 AppIntent
+struct ReceiveMessageIntent: AppIntent {
+    static let title: LocalizedStringResource = "기지국 위치정보 저장하기"
+    static let description = IntentDescription("전달된 문자 메시지에 포함된 주소를 추출하여 케이스에 저장합니다.")
+    
+    @MainActor
+    func perform() async throws -> some IntentResult {
+        return .result()
+    }
+}
