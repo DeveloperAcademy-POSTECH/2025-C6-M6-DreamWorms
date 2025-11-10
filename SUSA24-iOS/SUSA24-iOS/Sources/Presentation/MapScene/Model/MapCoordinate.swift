f87045e21a58ec56fd55f182e6cb19dd8f775a19
Moo <mutopia82@gmail.com>
2025-11-11 00:32:45 +0900

    [Feat] #70 - MapCoordinator Sendable 프로토콜 추가
    What?
    - API 요청 타입과 겹쳐서 범용으로 사용할 수 있게 Sendable을 채택했습니다.
    
    좋지 못한가?

--- SUSA24-iOS/SUSA24-iOS/Sources/Presentation/MapScene/Model/MapCoordinate.swift
+++ SUSA24-iOS/SUSA24-iOS/Sources/Presentation/MapScene/Model/MapCoordinate.swift
@@ -8,7 +8,7 @@
 /// 지도 명령을 전달하기 위한 좌표 값입니다.
 /// `NMFMapView`가 사용하는 위도/경도 순서를 그대로 보존해 명령 디스패처가
 /// 다른 모듈과 안전하게 좌표 정보를 교환할 수 있도록 돕습니다.
-struct MapCoordinate: Equatable {
+struct MapCoordinate: Equatable, Sendable {
     /// 이동 대상 위치의 위도입니다.
     let latitude: Double
     /// 이동 대상 위치의 경도입니다.
