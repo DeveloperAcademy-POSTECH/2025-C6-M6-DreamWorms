1e8e191d2cb93a97612db53b6b1049fa7218c324
mini-min <mini@10-141-62-88.0200.pos.kr.iosda.org>
2025-11-11 11:59:38 +0900

    [Feat] #93 - CaseListEmpty 화면 추가
    What?
    - CaseListEmpty 구현
    - 관련 Asset Files, StringLiterals 항목 추가

---
+++ SUSA24-iOS/SUSA24-iOS/Sources/Presentation/CaseListScene/SubViews/CaseListEmpty.swift
@@ -0,0 +1,32 @@
+//
+//  CaseListEmpty.swift
+//  SUSA24-iOS
+//
+//  Created by mini on 11/11/25.
+//
+
+import SwiftUI
+
+struct CaseListEmpty: View {
+    var body: some View {
+        VStack(spacing: 26) {
+            Image(.imgEmpty)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 70)
+            
+            VStack(spacing: 4) {
+                Text(.caseListEmptyTitle)
+                    .font(.titleSemiBold16)
+                    .foregroundStyle(.labelNeutral)
+                Text(.caseListEmptyDescription)
+                    .font(.bodyMedium14)
+                    .foregroundStyle(.labelAssistive)
+            }
+        }
+    }
+}
+
+//#Preview {
+//    CaseListEmpty()
+//}
