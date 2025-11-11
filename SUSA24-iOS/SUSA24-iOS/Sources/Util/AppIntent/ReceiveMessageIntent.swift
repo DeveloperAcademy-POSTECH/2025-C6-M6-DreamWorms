ac9f2a750943d68d9fc0f866b44744287163014d
G.M_YOO <ykm7003@gmail.com>
2025-11-11 11:48:27 +0900

    [Feat] #85 구현 고도화후 App Intent 단위 테스트와 유닛테스트 코드 구현
    why ?
    
    - 코드상으로 App Intent를 테스트하기엔 어려움이 있어 AI를 활용한 테스트 코드 작성

--- SUSA24-iOS/SUSA24-iOS/Sources/Util/AppIntent/ReceiveMessageIntent.swift
+++ SUSA24-iOS/SUSA24-iOS/Sources/Util/AppIntent/ReceiveMessageIntent.swift
@@ -26,7 +26,6 @@ struct ReceiveMessageIntent: AppIntent {
     
     @MainActor
     func perform() async throws -> some IntentResult {
-        
         // Repository 생성
         let context = PersistenceController.shared.container.viewContext
         let caseRepository = CaseRepository(context: context)
